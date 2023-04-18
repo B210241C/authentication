7726097e4d1ac63c245db56aee34b6f05949e2de
QwertyLIM <limyj2000222@gmail.com>
2023-04-18 23:54:52 +0800

    remove things no need

--- lib/main.dart
+++ lib/main.dart
@@ -1,140 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:authentication/login_widget.dart';
+import 'package:authentication/login.dart';
+import 'package:authentication/register.dart';
+import 'package:authentication/home.dart';
 
-Future main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+void main() {
+  runApp(ExpenseApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class ExpenseApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Expense Tracker'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            padding: EdgeInsets.all(20.0),
-            color: Colors.blue,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'New Expense',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Amount',
-                    labelStyle: TextStyle(color: Colors.white),
-                  ),
-                  style: TextStyle(color: Colors.white),
-                ),
-                SizedBox(height: 10.0),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Description',
-                    labelStyle: TextStyle(color: Colors.white),
-                  ),
-                  style: TextStyle(color: Colors.white),
-                ),
-                SizedBox(height: 10.0),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Date',
-                    labelStyle: TextStyle(color: Colors.white),
-                  ),
-                  style: TextStyle(color: Colors.white),
-                ),
-                SizedBox(height: 20.0),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Save'),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Overview',
-                          style: TextStyle(
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 20.0),
-                        Text(
-                          'Total Expenses: \$80.00',
-                          style: TextStyle(fontSize: 18.0),
-                        ),
-                        SizedBox(height: 10.0),
-                        Text(
-                          'Last Expense: \$50.00 on 04/14/2023',
-                          style: TextStyle(fontSize: 18.0),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(20.0),
-                    color: Colors.grey[200],
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Tips',
-                          style: TextStyle(
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 20.0),
-                        Text(
-                          'Tip 1: Set a budget for each category to avoid overspending.',
-                          style: TextStyle(fontSize: 18.0),
-                        ),
-                        SizedBox(height: 10.0),
-                        Text(
-                          'Tip 2: Keep track of your expenses daily to avoid surprises at the end of the month.',
-                          style: TextStyle(fontSize: 18.0),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
+  Widget build(BuildContext context){
+    return MaterialApp(
+      initialRoute: MyLogin.id,
+      routes: {
+        MyLogin.id: (context) => MyLogin(),
+        MyRegister.id: (context) => MyRegister(),
+        Myhome.id:(context) => Myhome(),
+      },
     );
   }
 }
-
-
-
-
-
-
-
 
